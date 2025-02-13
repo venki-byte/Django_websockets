9988b37f79b2a1f1a24c7c469c31c4bf26f9283f
venkat <venkatakrishnan2222@gmail.com>
2025-02-13 14:48:57 +0530

    first commit

---
+++ chat/consumers.py
@@ -0,0 +1,37 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = "test"
+
+        # Add this channel to the group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        # Accept WebSocket connection
+        await self.accept()
+
+    async def receive(self, text_data):  # Make this async
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Send message to group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+    
+    async def chat_message(self, event):  # Make this async
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'type': 'chat',
+            'message': message
+        }))
